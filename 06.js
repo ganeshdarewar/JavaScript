c86f53835b8ed5d75cc3c3d4e03613061d8ef0c6
ganeshdarewar <ganeshdarewar@gmail.com>
2024-02-14 20:21:59 +0530

    Math and Number methods

---
+++ 06.js
@@ -0,0 +1,37 @@
+// Number and Maths
+
+// Number
+// const balance=new Number(23.89);
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+// console.log(balance.toPrecision(3))
+
+// let hundred=100000;
+
+// console.log(hundred.toLocaleString('en-IN'));
+
+
+
+// ++++++++++maths
+// console.log(Math);
+
+// console.log(Math.abs(-1));
+// console.log(Math.round(4.7));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.2));
+// console.log(Math.sqrt(4));
+// console.log(Math.max(4.2, 2.3, 4));
+// console.log(Math.min(4.2, 2.3, 4));
+
+// console.log(Math.random()); // 0 to 1
+
+// console.log(Math.floor((Math.random()*10))+1); 
+
+let min=10
+let max=20
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
+
+
+
