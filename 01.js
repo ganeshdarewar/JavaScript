66fdf69e700f479430af2449388b19305ddcfb61
ganeshdarewar <ganeshdarewar@gmail.com>
2024-02-14 12:37:37 +0530

    data type

---
+++ 01.js
@@ -0,0 +1,20 @@
+
+// let const var
+
+const accountId=12; //cant change after defining
+let accountEmail="Ganeshdarewar@gmaail.com"
+var accountPassword="121331";
+let accountState; //undefined
+
+accountEmail="absjb"
+
+// console.log(accountId);
+// console.log(accountEmail);
+// console.log(accountPassword);
+
+console.table([accountId, accountEmail, accountPassword, accountState])
+
+/*
+Note :
+Do not use var keyword because of issues in block scope and functional scope
+*/
---
+++ 02.js
@@ -0,0 +1,18 @@
+// datatypes
+
+// "use strict"; // treat all js code as newer version of js
+
+
+// Number
+// BigInt for big Number
+// String => ""
+// Boolean => true/ false
+// null => standalone value
+// undefined=> variable declare but not not defined value
+// Symbol => (Uniquness)for react
+
+// object
+console.log(typeof "ganesh") 
+console.log(typeof null); // null => object or language error
+
+
