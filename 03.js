4b9808aff319016daf29c7ff0dbac9410494eb26
ganeshdarewar <ganeshdarewar@gmail.com>
2024-02-14 13:42:26 +0530

    conversion of datatypes

---
+++ 03.js
@@ -0,0 +1,72 @@
+// conversion and operation
+
+// let score="3"
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInnumber=Number(score);  // typecasting
+// console.log(typeof valueInnumber);
+
+// let score="3abc"
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInnumber=Number(score);  // typecasting
+// console.log(typeof valueInnumber);
+// console.log(valueInnumber); // => NaN(not a number)
+
+
+// let score=null
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInnumber=Number(score);  // typecasting
+// console.log(typeof valueInnumber);
+// console.log(valueInnumber); // => 0
+
+
+// let score=undefined
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInnumber=Number(score);  // typecasting
+// console.log(typeof valueInnumber);
+// console.log(valueInnumber); // => Nan
+
+// let score=true
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInnumber=Number(score);  // typecasting
+// console.log(typeof valueInnumber);
+// console.log(valueInnumber); // => 1
+
+// let score="Ganesh"
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInnumber=Number(score);  // typecasting
+// console.log(typeof valueInnumber);
+// console.log(valueInnumber); // => Nan
+/*
+typecasting to number from different data
+"33"=> 33
+"33abc"=>NaN
+true=>1
+
+*/
+
+// let loggedIn=1
+// let isLoggedIn=Boolean(loggedIn);
+// console.log(isLoggedIn);
+// let loggedIn="" //=>false
+// let isLoggedIn=Boolean(loggedIn);
+// console.log(isLoggedIn);
+
+// let loggedIn="Ganesh"//true
+// let isLoggedIn=Boolean(loggedIn);
+// console.log(isLoggedIn);
+
+let someNumber=23
+let stringNumber=String(someNumber);
+console.log(someNumber);
